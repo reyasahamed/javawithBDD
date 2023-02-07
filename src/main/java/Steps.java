444c057984b46c5c7f9cb057450ff62c0fed47a7
reyasahamed <reyas.ahmed99@gmail.com>
2023-02-07 23:10:05 +0530

    Initial commit

---
+++ src/main/java/Steps.java
@@ -0,0 +1,13 @@
+public class Steps {
+    @io.cucumber.java.en.Given("^Open the Firefox and launch the application$")
+    public void openTheFirefoxAndLaunchTheApplication() {
+    }
+
+    @io.cucumber.java.en.When("^Enter the Username and Password$")
+    public void enterTheUsernameAndPassword() {
+    }
+
+    @io.cucumber.java.en.Then("^Reset the credential$")
+    public void resetTheCredential() {
+    }
+}
